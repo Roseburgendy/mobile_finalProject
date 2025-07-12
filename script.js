e2469260c5faf92b74f04881f9e52fef78bc9b44
Roseburgendy <rosewuxi@gmail.com>
2025-07-12 17:56:43 +0800

    7.12 ui小部分完成

---
+++ script.js
@@ -0,0 +1,23 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+window.addEventListener("scroll", function () {
+  const header = document.getElementById("header-main");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
